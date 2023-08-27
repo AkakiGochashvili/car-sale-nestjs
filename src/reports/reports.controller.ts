b5051338c083435fdae43b2fa0cc3a27b19130d9
Akaki Gochashvili <akakigochashvili16@gmail.com>
2023-08-27 20:06:19 +0400

    feat: create report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { createReportDto } from './dtos/create-repost.dto';
+import { ReportsService } from './reports.service';
+import { JwtProtect } from '../common/Guards/auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Reports')
+@ApiBearerAuth()
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+	constructor(private reportsService: ReportsService) {}
+
+	@JwtProtect()
+	@Post()
+	createRepost(@Body() body: createReportDto) {
+		return this.reportsService.create(body);
+	}
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Report } from './report.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { createReportDto } from './dtos/create-repost.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+	constructor(@InjectRepository(Report) private repository: Repository<Report>) {}
+
+	async create(report_data: createReportDto) {
+		const report_instance = this.repository.create(report_data);
+
+		const report = await this.repository.save(report_instance);
+
+		return report;
+	}
+}
